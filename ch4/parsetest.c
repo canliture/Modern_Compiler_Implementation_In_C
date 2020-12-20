a0018a2a8b5c7373c17b67e7f5b7099ebf5580de
canliture <canliture@outlook.com>
2020-12-20 17:35:02 +0800

    enable printing syntax

--- ch4/parsetest.c
+++ ch4/parsetest.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "parse.h"
+#include "prabsyn.h"
 
 int main(int argc, char **argv) {
     if (argc!=2) {fprintf(stderr,"usage: a.out filename\n"); exit(1);}
     A_exp exp = parse(argv[1]);
     if (exp) {
         fprintf(stderr, "Parsing successful!\n");
+        pr_exp(stdout, exp, 4);
+        printf("\n");
     } else {
         fprintf(stderr,"Parsing failed\n");
     }
